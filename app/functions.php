5e76eb272b3a31ef14e55cf0064640d93c1ce20a
LIU JIAN <onanying@126.com>
2020-01-08 17:14:11 +0800

    修改 app(), context() 为骨架内函数

--- app/functions.php
+++ app/functions.php
@@ -2,6 +2,7 @@
 
 /**
  * 用户助手函数
+ * @author liu,jian <coder.keda@gmail.com>
  */
 
 /**
@@ -11,4 +12,26 @@
 function get_phar_dir()
 {
     return str_replace(['phar://', '/'], ['', DIRECTORY_SEPARATOR], dirname(dirname(__DIR__)));
+}
+
+/**
+ * 获取全局App对象
+ * @return \Mix\Console\Application
+ */
+if (!function_exists('app')) {
+    function app()
+    {
+        return $GLOBALS['app'];
+    }
+}
+
+/**
+ * 获取全局上下文对象
+ * @return \Mix\Bean\ApplicationContext
+ */
+if (!function_exists('context')) {
+    function context()
+    {
+        return app()->context;
+    }
 }
